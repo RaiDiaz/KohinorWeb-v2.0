4dc54e9821dfb34a25d2f2ce67b4352b2b528860
RaiDiaz <76229344+RaiDiaz@users.noreply.github.com>
2022-12-20 17:43:18 -0500

    Add files via upload

---
+++ KohinorERP/Models/TestMenu.cs
@@ -0,0 +1,15 @@
+﻿namespace KohinorERP.Models
+{
+    public class TestMenu
+    {
+        public List<Menu> MenuList { get; set; }
+    }
+    public class Menu
+    {
+        public int id { get; set; }
+        public string nameMenu { get; set; }
+        public string url { get; set; }
+        public string name { get; set; }
+        public string icon { get; set; }
+    }
+}
